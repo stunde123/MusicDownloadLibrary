9ee67a5427b13a8cec0d71553c1ae62755bda244
xhw <673123467@qq.com>
2022-08-11 14:52:39 +0800

    111

--- app/src/main/java/com/example/musiclibrary/MainActivity.java
+++ app/src/main/java/com/example/musiclibrary/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import api.music.download.MusicApi;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MusicApi.getInstance().init(this);
     }
 }
